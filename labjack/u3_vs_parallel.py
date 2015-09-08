500ae24f11f8775d83b1ecf2a767be44704f65c5
Christopher J. Bailey <bailey.cj@gmail.com>
2015-09-08 13:29:34 +0200

    added sounds to test latency

--- labjack/u3_vs_parallel.py
+++ labjack/u3_vs_parallel.py
@@ -8,10 +8,32 @@ is to read the code:
 
 https://github.com/labjack/LabJackPython/blob/master/src/u3.py
 """
-from psychopy import core, logging, gui, event, visual
+from psychopy import core, logging, gui, event, visual, sound, prefs
 
 from psychopy import parallel
 from labjack import u3
+
+Fs = 44100
+bufferLen = 128
+### Trig-to-sound delay measured with scope
+##  MacBook Pro 13 late 2013
+# 128: [22.5, 25.5], no audible distortion
+#  64: [21.5, 22.5], no audible distortion
+#  32: [20.6, 21.2], no audible distortion
+#    : smaller values give terrible distortion
+
+if prefs.general['audioLib'][0] == 'pyo':
+    #if pyo is the first lib in the list of preferred libs then we could use small buffer
+    #pygame sound is very bad with a small buffer though
+    sound.init(Fs,buffer=bufferLen)
+    
+print 'Using %s(with %s) for sounds' %(sound.audioLib, sound.audioDriver)
+
+highA = sound.Sound('A',octave=3, sampleRate=Fs, secs=0.8, bits=8)
+highA.setVolume(0.8)
+highC = sound.Sound('C',octave=4, sampleRate=Fs, secs=0.4, bits=8)
+tick = sound.Sound(800,secs=0.01,sampleRate=Fs, bits=8)#sample rate ignored because already set
+tock = sound.Sound('600',secs=0.01, sampleRate=Fs)
 
 parallel.setPortAddress(0x378)#address for parallel port on many machines
 # Figure out whether to flip pins or fake it
@@ -43,12 +65,12 @@ u3dev = u3.U3()
 # to see whether the EIO channels are by default OUTputs
 # To change, maybe u3dev.exportConfig() to file, then
 # u3dev.loadConfig() to apply?
-# Perhaps initial state can also be saved to chip:
+# Perhaps initial state can also be saved to chip (by default they are all UP!)
 u3dev.getFeedback(u3.PortStateWrite([0x00, 0x00, 0x00]))
 
 def trigger_u3(code=1, trigDuration=0.010):
     trigUpCmd = u3.PortStateWrite([0x00, code, 0x00])
-    trigDurCmd = u3.WaitShort(int(Time=trigDuration/128.e-6))
+    trigDurCmd = u3.WaitShort(Time=int(trigDuration/128.e-6))
     trigDownCmd = u3.PortStateWrite([0x00, 0x00, 0x00])
 
     u3dev.getFeedback([trigUpCmd, trigDurCmd, trigDownCmd])
@@ -83,6 +105,10 @@ frameSyncSpot = visual.GratingStim(win,tex=None, mask="gauss",
 
 bContinue = True
 curCode = 1
+
+highA.play()
+core.wait(0.8)
+
 while bContinue:
 
 #    rad_stim.setRadialPhase(radialPhaseAdvanceBaseline, operation='-')
@@ -90,6 +116,10 @@ while bContinue:
 
     trigger_parallel(code=curCode)
     trigger_u3(code=curCode<<4)
+#    tick.play()
+#    core.wait(0.4)
+    highC.play()
+    core.wait(0.4)
 
     for _ in xrange(int(frameRate//10)):
         frameSyncSpot.draw()
@@ -99,6 +129,8 @@ while bContinue:
     # trigger_parallel(code=0)
     # trigger_u3(code=0)
 
+#    tock.play()
+#    core.wait(0.6)
     for _ in xrange(int(frameRate//10)):
         win.flip()  # disappear
 
