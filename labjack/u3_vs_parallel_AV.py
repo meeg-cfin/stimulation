4039396c9a8b696c52826add6194b02dedf81f6c
Christopher J. Bailey <bailey.cj@gmail.com>
2015-12-09 10:44:21 +0100

    from MEG, audio broken

--- labjack/u3_vs_parallel_AV.py
+++ labjack/u3_vs_parallel_AV.py
@@ -3,9 +3,13 @@
 Test trigger latency of LabJack U3-LV (USB) vs. parallel port
 This will only work on a suitably ancient Win-machine (XP, 32-bit)
 """
-from psychopy import core, logging, event, visual
-from psychopy import parallel
+from psychopy import core, logging, event, visual, sound
+from psychopy import parallel, prefs
 from labjack import u3
+import sys
+# This is ugly code, revise by making wavhelpers part of a module
+sys.path.insert(0, '../')
+import utilities
 
 when = core.Clock()
 
@@ -20,26 +24,46 @@ except RuntimeError:
                   format(when.getTime(), code))
         pass
 else:
-    setParallelData = parallel.setData
+    def setParallelData(code=0):
+        parallel.setData(code)
     setParallelData(0)
 
 try:
     u3dev = u3.U3()
-except:  # NullHandleException not known to Flake8
+except Exception as e:  # NullHandleException not known to Flake8
+    print(e)
     def setU3Data(code=0):
         if code > 0:
             print('{:.7f} sec, U3 TRIG {:d} (Fake)'.
                   format(when.getTime(), code))
-        pass
 else:
     def setU3Data(code=0):
         u3dev.getFeedback(u3.PortStateWrite([0x00, code, 0x00]))
-
+    setU3Data(0)
+    
 # Not needed:
 #    core.wait(trigDuration,trigDuration/2.) # equivalent to time.sleep(0.005) plus a tight while-loop for another 0.005 secs!
 #    core.wait(trigDuration)
 #    u3dev.getFeedback(u3.PortStateWrite([0x00, 0x00, 0x00]))
 #    u3dev.toggleLED()
+
+
+if prefs.general['audioLib'][0] == 'pyo':
+    #if pyo is the first lib in the list of preferred libs then we could use small buffer
+    #pygame sound is very bad with a small buffer though
+    sound.init(48000,buffer=128)
+print 'Using %s(with %s) for sounds' %(sound.audioLib, sound.audioDriver)
+
+audioSamplingRate = 48000
+audStimDur_sec = 0.050
+audStimTaper_sec = 0.005
+
+# Using wavhelpers:
+monoChanStr = \
+    utilities.load_stimuli(200.0, audioSamplingRate,
+                           audStimDur_sec, audStimTaper_sec, isStereo=False)
+
+monoSound = sound.Sound(monoChanStr, autoLog=False)
 
 
 # Window and stimulus definitions
@@ -66,6 +90,8 @@ frameSyncSpot = visual.GratingStim(win, tex=None, mask=None,
                                    units='deg', pos=(0, 0), autoLog=False)
 #####################
 
+spotOnTime = int(frameRate//20)  # trigger should be on for 3 frames, i.e., 50 ms
+spotOffTime = int(frameRate - spotOnTime)
 
 bContinue = True
 curCode = 1
@@ -75,16 +101,19 @@ while bContinue:
 #    rad_stim.draw()
 
     setParallelData(code=curCode)
-    setU3Data(code=curCode<<4)
+    # setU3Data(code=curCode<<4)
+    setU3Data(code=curCode)
 
-    for _ in xrange(int(frameRate//20)):  # trigger should be on for 3 frames, i.e., 50 ms
+    monoSound.play()
+
+    for _ in xrange(spotOnTime):  
         frameSyncSpot.draw()
         win.flip()  # appear
 
     setParallelData(code=0)
     setU3Data(code=0)
 
-    for _ in xrange(int(frameRate//10)):  # off for 100 ms
+    for _ in xrange(spotOffTime):
         win.flip()  # disappear
 
     if event.getKeys(keyList=['escape', 'q', 'space']):  # flush it!
