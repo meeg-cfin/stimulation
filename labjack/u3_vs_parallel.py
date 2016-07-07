ac9c8159f411897009e6cf0e14cf59583a3167af
Christopher J. Bailey <bailey.cj@gmail.com>
2016-07-07 22:08:53 +0200

    from new stimpc, testing

--- labjack/u3_vs_parallel.py
+++ labjack/u3_vs_parallel.py
@@ -8,7 +8,7 @@ from psychopy import core, logging, gui, event, visual
 from psychopy import parallel
 from labjack import u3
 
-parallel.setPortAddress(0x378)#address for parallel port on many machines
+parallel.setPortAddress(0xDFF8)#address for parallel port on many machines
 # Figure out whether to flip pins or fake it
 try: parallel.setData(1)
 except RuntimeError:
@@ -35,18 +35,36 @@ def trigger_parallel(code=1,trigDuration=0.010):
 
 u3dev = u3.U3()
 u3dev.getFeedback(u3.PortStateWrite([0x00, 0x00, 0x00]))
+u3.LED(False)
 
-def trigger_u3(code=1, trigDuration=0.010):
+# u3dev.debug = True
+# print(u3dev.configU3())
+# u3dev.getFeedback(u3.BitDirRead(4), u3.BitDirWrite(4, 0), u3.BitDirRead(4))
+# print(u3dev.configU3())
+#u3dev.getFeedback(u3.PortDirRead())
+
+
+def trigger_U3(code=1):
     u3dev.getFeedback(u3.PortStateWrite([0x00, code, 0x00]))
 #    core.wait(trigDuration,trigDuration/2.) # equivalent to time.sleep(0.005) plus a tight while-loop for another 0.005 secs!
 #    core.wait(trigDuration)
 #    u3dev.getFeedback(u3.PortStateWrite([0x00, 0x00, 0x00]))
 #    u3dev.toggleLED()
 
+# FUCK
+#def trigger_U3(code=1, duration=0.10):
+#    cmd_list = [u3.PortStateWrite([0x00, code, 0x00]),
+#                u3.LED(True),
+#                u3.WaitShort(Time=int(duration/128.e-6)),
+#                u3.PortStateWrite([0x00, 0, 0x00]),
+#                u3.LED(False)]
+#    u3dev.getFeedback(cmd_list)
+
 ###### Window and stimulus definitions
 bckColour = '#303030'
+bckColour = '#000000'
 monitor = 'testMonitor'
-frameRate  = 60.0
+frameRate  = 120.0
 stimSize = 8.
 stimCycPerDeg = 1.
 angCycles = 0.
@@ -74,17 +92,20 @@ while bContinue:
 #    rad_stim.setRadialPhase(radialPhaseAdvanceBaseline, operation='-')
 #    rad_stim.draw()
 
+    trigger_U3(code=curCode<<4)
     trigger_parallel(code=curCode)
-    trigger_u3(code=curCode<<4)
+#    trigger_U3(code=curCode<<4, duration=0.100)
     
-    for _ in xrange(int(frameRate//10)):
+#    for _ in xrange(int(frameRate//10)):
+    # loop two frames
+    for _ in xrange(2):
         frameSyncSpot.draw()
         win.flip()  # appear
 
+    trigger_U3(code=0)
     trigger_parallel(code=0)
-    trigger_u3(code=0)
     
-    for _ in xrange(int(frameRate//10)):
+    for _ in xrange(int(frameRate//2)):
         win.flip()  # disappear
 
     if event.getKeys(keyList=['escape','q','space']): # flush it! 
